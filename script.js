721398b5534f264e845554b47ce01e624a0b8918
tapashrout29-byte <tapashrout29@gmail.com>
2026-08-06 14:04:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const buttons = document.querySelectorAll("button");
 
-  buttons.forEach((button) => {
-    button.addEventListener("click", function () {
-      alert("Welcome to Rout Raid! Booking feature is coming soon.");
-    });
-  });
-});
